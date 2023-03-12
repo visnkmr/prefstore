e5254e42127098a772ccb8cb11f4e732f06945de
12core1 <test@someone.local>
2023-03-12 20:18:11 -0200

    foldername fix
    foldername can be set to appname

--- prefstore/src/lib.rs
+++ prefstore/src/lib.rs
@@ -10,28 +10,70 @@ fn saveany<T>(x: T)->T {
     // return 
     x
 }
+// pub struct prefstore {
+// 	/// App name under which app settings are typically to be stored.
+// 	pub app: String,
+// }
+//  pub enum prefstore{
+//     Name(String)
+// }
+// impl name for prefstore{
+    
+// }
 
-pub fn savepreference<T: ToString>(key: impl Into<String>,value:T){
+pub fn setname(appname:String)->String{
+    appname
+}
+// impl ems<String> for String{}
+// trait name{
+//     fn savepreference<T: ToString>(key: impl Into<String>,value:T);
+//     fn default_name(filename:String) -> String;
+//     fn config_path(filename:impl Into<String>) -> PathBuf;
+//     fn getpreference<T:ToString>(key:impl Into<String>,defvalue:T)->String;
+//     // APPNAME:String;
+//  }
+ 
+ 
+//  pub struct setname{
+ 
+//  }
+//  impl name for setname{
+//  const fn setname(appn:String){
+    
+//  }
+// pub struct prefstore {
+// 	/// App name under which app settings are typically to be stored.
+// 	pub app: String
+// }
+// impl prefstore {
+
+//     pub fn setappname(nameofapp:String){
+//         app=nameofapp
+//     }
+// }
+
+pub fn savepreference<T: ToString>(app_name:impl Into<String>,key: impl Into<String>,value:T){
     let key=key.into();
+    let app_name=app_name.into();
     // println!("{}",config_path(key.into()).to_string_lossy());
     //create new file named key and save value
-    create_dir_all(&config_path(&key).parent().expect("Cannot find some path to create config")).expect("cannot create dirs necessary to create config");
-    write!(File::create(&config_path(&key)).expect("Cannot create file."), "{}", value.to_string());
-    println!("saved successfully to {}",config_path(&key).to_string_lossy())
+    create_dir_all(&config_path(&app_name,&key).parent().expect("Cannot find some path to create config")).expect("cannot create dirs necessary to create config");
+    write!(File::create(&config_path(&app_name,&key)).expect("Cannot create file."), "{}", value.to_string());
+    println!("saved successfully to {}",config_path(&app_name,&key).to_string_lossy())
 }
 
 fn default_name(filename:String) -> String {
     format!("{}.txt", filename).to_lowercase()
 }
 
-fn config_path(filename:impl Into<String>) -> PathBuf {
+fn config_path(app_name:&String,filename:impl Into<String>) -> PathBuf {
 
     let app= var("CARGO_PKG_NAME").unwrap_or_else(|_| env!("CARGO_PKG_NAME").to_string());
     
     match(dirs::config_dir()){
         Some(system_config_dir) =>{
             system_config_dir
-                    .join(app)
+                    .join(app_name)
                     .join(default_name(filename.into()))
         },
         None => {
@@ -41,23 +83,26 @@ fn config_path(filename:impl Into<String>) -> PathBuf {
     }
 }
 
-pub fn getpreference<T:ToString>(key:impl Into<String>,defvalue:T)->String{
+pub fn getpreference<T:ToString>(app_name:impl Into<String>,key:impl Into<String>,defvalue:T)->String{
     //Try to open file named key, If not found return defvalue
     use io::Read;
     let key =key.into();
+    let app_name =app_name.into();
     // let defvalue=defvalue.into();
-				match(File::open(&config_path(&key))){
+				match(File::open(&config_path(&app_name,&key))){
                     Ok(mut file) => {
                         let mut buf = String::new();
                         file.read_to_string(&mut buf).expect("Cannot read to string");
                         buf
                     },
                     Err(_) => {
-                        savepreference(&key, &defvalue.to_string());
+                        savepreference(app_name,&key, &defvalue.to_string());
                         defvalue.to_string()
                     },
                 }
 }
+
+// }
 
 pub trait ems<T>{
     fn tobool(self)->bool;
@@ -79,6 +124,5 @@ impl ems<String> for String{
     fn toi32(self)->i32{
         self.parse::<i32>().unwrap()
     }
-
+    
 } 
- 
--- ps_example/src/main.rs
+++ ps_example/src/main.rs
@@ -9,7 +9,7 @@ use std::path::Path;
 use std::process;
 use std::thread;
 use std::time::Duration;
-use chrono::Local;
+// use chrono::Local;
 use prefstore;
 use prefstore::ems;
 use prefstore::getpreference;
@@ -22,36 +22,39 @@ use prefstore::savepreference;
 [f64]
  */
 
+
+
 fn main() {
-   println!("hello");
-   savepreference("boolean",false);
+   const APPNAME:&str="nameofapp";
+   // println!("hello");
+   savepreference(APPNAME,"boolean",false);
 
    let set=9;
-   savepreference("i32",set);
+   savepreference(APPNAME,"i32",set);
 
    let set=6.8;
-   savepreference("f64",set);
+   savepreference(APPNAME,"f64",set);
    
    let set:i128=99999999999999999999999999999999999999;
    
-   savepreference("verylongi",set);
+   savepreference(APPNAME,"verylongi",set);
 //    let mut places = vec!["Paris", "New York"];
 //     places.push("Madrid");
 //     places.push("Toronto");
 //    let set=["test","try"];
 //    savepreference("strarr",places);
-   println!("test");
-   thread::spawn(move || loop {
-      // println!("fromhere------------>1");
-      let date = Local::now();
-      let current_date = date.format("%Y-%m-%d").to_string();
-      let k= getpreference(&current_date,0 as u128).parse::<u128>().unwrap();
-      
-      println!("{}",getpreference(&current_date,0 as u128).parse::<u128>().unwrap());
+//    println!("test");
+//    thread::spawn(move || loop {
+//       // println!("fromhere------------>1");
+//       let date = Local::now();
+//       let current_date = date.format("%Y-%m-%d").to_string();
+//       let k= getpreference(&current_date,0 as u128).parse::<u128>().unwrap();
 
-      thread::sleep(Duration::from_secs(60));
-  });
+//       println!("{}",getpreference(&current_date,0 as u128).parse::<u128>().unwrap());
+
+//       thread::sleep(Duration::from_secs(60));
+//   });
 
   
-   println!("{}",getpreference("start",false).tobool());
+   println!("{}",getpreference(APPNAME,"start",false).tobool());
 }
